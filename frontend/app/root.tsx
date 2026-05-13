25a8a56e9f3d8c11b98dbf7d767aea2a46b977bd
Gabidanety <gabinete878@gmail.com>
2026-05-13 19:21:22 -0300

    frontend configurado

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -7,69 +7,45 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import type { Route } from "./+types/root";
-import "./app.css";
+import "./styles/app.css";
+import {useEffect} from "react";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export default function Root() {
 
-export function Layout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const daltonicoMode = typeof window !== "undefined" && localStorage.getItem("daltonicMode")==="true";
+      if(daltonicoMode){''
+        document.body.classList.add("daltonic-mode");
+      }else{
+        document.body.classList.remove("daltonic-mode");
+      }
+
+  },[]);
+
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+                <title>EcoRecicla</title>
+
+        <link rel= "preconnect" href= "https://fonts.googleapis.com" />
+        <link rel= "preconnect" href="https://fonts.gstatic.com" crossOrigin= ""/>
+        <link rel = "stylesheet" href = "https://fonts.googleapis.com/css2?family=Inter:wght400;500;600&family=Sora:wght@400;600;700;800&display=swap" />
+
       </head>
       <body>
-        {children}
+        <div className="app-wrapper">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
-  );
-}
+  
+    
+  );}
 
-export default function App() {
-  return <Outlet />;
-}
 
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
-
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  );
-}
---
+++ frontend/app/routes.js
@@ -0,0 +1,5 @@
+import { route, index } from "@react-router/dev/routes";
+
+export default [
+    index("routes/home.tsx")
+];
